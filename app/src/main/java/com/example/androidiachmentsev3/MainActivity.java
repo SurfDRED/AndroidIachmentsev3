3369a236be7cf69d8d2934ba8b82bd76fc21614a
Dmitrii Iachmentcev <d.yachmentsev@gmail.com>
2020-02-05 21:07:55 +0300

    Исправил ошибки

--- app/src/main/java/com/example/androidiachmentsev3/MainActivity.java
+++ app/src/main/java/com/example/androidiachmentsev3/MainActivity.java
@@ -7,44 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        onClicker();
-        onClear();
-    }
-    public void onClicker() {
-        // Обработка кнопки ОК
+// найдем View-элементы
+        final TextView ePolzovatel = findViewById(R.id.edPolzovatel);
+        final TextView eEmail = findViewById(R.id.edEmail);
+        final TextView edinfo = findViewById(R.id.edInfo);
         Button exampleBtn = findViewById(R.id.buttonOk);
+        Button exampleBtn2 = findViewById(R.id.buttonClear);
+// обработка кнопки Ок
         exampleBtn.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                TextView ePolzovatel = findViewById(R.id.edPolzovatel);
-                TextView eEmail = findViewById(R.id.edEmail);
-                TextView edinfo = findViewById(R.id.edInfo);
                 edinfo.setText(String.format("Подписка на рассылку успешно оформлена для пользователя %S на электронный адрес: %s", ePolzovatel.getText(), eEmail.getText()));
             }
         });
-    }
-
-    public void onClear() {
-        // Обработка кнопки Очистить
-        Button exampleBtn2 = findViewById(R.id.buttonClear);
+// обработка кнопки Clear
         exampleBtn2.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                TextView ePolzovatel = findViewById(R.id.edPolzovatel);
                 ePolzovatel.setText("");
-                TextView eEmail = findViewById(R.id.edEmail);
                 eEmail.setText("");
-                TextView edinfo = findViewById(R.id.edInfo);
                 edinfo.setText("");
             }
         });
